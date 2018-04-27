bd6a7977487860f4e3268cfe74d46ce6dc058538
neochrome <johan@stenqvist.net>
2018-04-27 10:11:20 +0200

    Add records based debugger with example
    Signed-off-by: Johan Stenqvist <johan@stenqvist.net>

--- test/test_client.ml
+++ test/test_client.ml
@@ -1,5 +1,6 @@
 let counter = let open Test_client_counter in main
 let counter_debug = let open Test_client_counter_debug in main
+let counter_debug_records = let open Test_client_counter_debug_records in main
 let btn_update_span = let open Test_client_btn_update_span in main
 let attribute_removal = let open Test_client_attribute_removal in main
 let drag = let open Test_client_drag in main
