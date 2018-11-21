d5564248eef8266c7abe8c3dd401b20c050591cd
Paul Biggar <paul.biggar@gmail.com>
2018-11-20 17:28:52 -0800

    Fix Tea.Html.Cmds.focus
    Co-authored-by: Ian Connolly <ian@connolly.io>

--- src-ocaml/tea_html_cmds.ml
+++ src-ocaml/tea_html_cmds.ml
@@ -1,11 +1,16 @@
-
 let focus id =
-  Tea_cmd.call (fun _enqueue -> match Js.Nullable.toOption (Web.Document.getElementById id) with
-      | None -> Js.log ("Attempted to focus a non-existant element of: ", id)
-      | Some elem ->
-        (* let () = Js.log ("Focusing element", id, elem) in *)
-        let ecb _ignored = Web.Node.focus elem in
-        let cb _ignored = let _unhandledID = Web.Window.requestAnimationFrame ecb in () in (* One to get out of the current render frame*)
-        let _unhandledID = Web.Window.requestAnimationFrame cb in (* And another to properly focus *)
-        ()
-    )
+  Tea_cmd.call
+    (fun _enqueue  ->
+       let ecb _ =
+         match Js.Nullable.toOption (Web.Document.getElementById id) with
+         | None  ->
+           Js.log ("Attempted to focus a non-existant element of: ", id)
+         | Some elem -> Web.Node.focus elem
+       in
+       (* One to get out of the current render frame*)
+       let cb _ = ignore (Web.Window.requestAnimationFrame ecb) in
+       (* And another to properly focus *)
+       ignore (Web.Window.requestAnimationFrame cb);
+       ())
+
+
--- src-reason/tea_html_cmds.re
+++ src-reason/tea_html_cmds.re
@@ -1,15 +1,14 @@
 let focus = id =>
-  Tea_cmd.call(_enqueue =>
-    switch (Js.Nullable.toOption(Web.Document.getElementById(id))) {
-    | None => Js.log(("Attempted to focus a non-existant element of: ", id))
-    | Some(elem) =>
-      /* let () = Js.log ("Focusing element", id, elem) in */
-      let ecb = _ignored => Web.Node.focus(elem);
-      let cb = _ignored => {
-        let _unhandledID = Web.Window.requestAnimationFrame(ecb);
-        ();
-      }; /* One to get out of the current render frame*/
-      let _unhandledID = Web.Window.requestAnimationFrame(cb); /* And another to properly focus */
-      ();
-    }
-  );
+  Tea_cmd.call(_enqueue => {
+    let ecb = _ =>
+      switch (Js.Nullable.toOption(Web.Document.getElementById(id))) {
+      | None => Js.log(("Attempted to focus a non-existant element of: ", id))
+      | Some(elem) => Web.Node.focus(elem)
+      };
+
+    /* One to get out of the current render frame*/
+    let cb = _ => ignore(Web.Window.requestAnimationFrame(ecb));
+    /* And another to properly focus */
+    ignore(Web.Window.requestAnimationFrame(cb));
+    ();
+  });
