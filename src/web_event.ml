94cbd2207b57b10882eb40822b9cf4280a5b31bb
Paul Biggar <paul.biggar@gmail.com>
2022-10-28 18:47:33 -0400

    Remove unused types

--- src/web_event.ml
+++ src/web_event.ml
@@ -2,10 +2,4 @@ type t = Dom.event
 
 type cb = t -> unit
 
-type options = bool (* false | true (* TODO:  Define a javascript record as another option *) *)
-
-
-type popstateEvent = <
-> Js.t
-
-type popstateCb = popstateEvent -> unit [@bs]
+type options = bool
